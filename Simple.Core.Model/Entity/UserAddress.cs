514eb40667d5e99b6bd36b3ff519429f739f3abc
kolaynurdan <kolaynurdan@gmail.com>
2021-03-14 21:46:29 +0300

    updated user

--- Simple.Core.Model/Entity/UserAddress.cs
+++ Simple.Core.Model/Entity/UserAddress.cs
@@ -6,17 +6,13 @@ using System.Threading.Tasks;
 
 namespace Simple.Core.Model.Entity
 {
-    public class UserAddress:EntityBase
+    public class UserAddress : EntityBase
     {
         public int UserId { get; set; }
         public User User { get; set; }
-
         public string Title { get; set; }
-
         public string City { get; set; }
-
         public string Address { get; set; }
-        public bool MyPropeIsActive { get; set; }
-
+        public bool IsActive { get; set; }
     }
 }
---
+++ Simple.Core.Model/Migrations/202103141753210_v2.cs
@@ -0,0 +1,22 @@
+﻿namespace Simple.Core.Model.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class v2 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Users", "Password", c => c.String());
+            AddColumn("dbo.UserAddresses", "IsActive", c => c.Boolean(nullable: false));
+            DropColumn("dbo.UserAddresses", "MyPropeIsActive");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.UserAddresses", "MyPropeIsActive", c => c.Boolean(nullable: false));
+            DropColumn("dbo.UserAddresses", "IsActive");
+            DropColumn("dbo.Users", "Password");
+        }
+    }
+}
